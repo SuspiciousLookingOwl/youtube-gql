b9d4a46c98d6d1fe92039a6d13e03543125ef5da
VincentJonathan <vinjongm@gmail.com>
2021-07-08 18:05:19 +0700

    add comment and video search

--- src/components/video.ts
+++ src/components/video.ts
@@ -22,12 +22,35 @@ export const schema = gql`
 		thumbnail: [String]!
 		uploadDate: String!
 		viewCount: Int
+		comments(limit: Int): [Comment]!
+		upNext: VideoCompact
+	}
+
+	type VideoCompact {
+		id: ID!
+		title: String!
+		channel: Channel!
+		thumbnail: [String]!
+		uploadDate: String
+		viewCount: Int
+	}
+
+	type Comment {
+		id: ID!
+		author: Channel!
+		content: String!
+		isAuthorChannelOwner: Boolean!
+		isPinnedComment: Boolean!
+		likeCount: Int
+		replyCount: Int
+		publishDate: String!
 	}
 `;
 
 export const query = gql`
 	extend type Query {
 		video(id: String!): Video
+		videos(keyword: String!, limit: Int): [VideoCompact]
 	}
 `;
 
@@ -37,8 +60,20 @@ export const queryResolver: IResolverObject = {
 		if (!video) return;
 		return video;
 	},
+
+	async videos(_, { keyword = "", limit = 10 }) {
+		const videos = await youtube.search(keyword, { type: "video" });
+		return videos.slice(0, limit);
+	},
 };
 
 export const resolvers: IResolverObject = {
 	channel: (v: Video) => channel.mapper(v.channel),
+	async comments(v: Video, { limit = 10 }) {
+		while (v.comments.length < limit) {
+			const comments = await v.nextComments();
+			if (!comments.length) break;
+		}
+		return v.comments.slice(0, limit);
+	},
 };
